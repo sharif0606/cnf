8b76a5c2080c81054c00f5223f815456b95483cb
Shariful Islam <ether_sb@yahoo.com>
2023-07-13 10:40:51 +0600

    map add

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -147,16 +147,10 @@
             </div>
         </div>
         <div class="col-sm-12 col-md-12 col-lg-12 pt-5">
-            <iframe
-              class="container-fluid footer-ifram"
-              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7379.432700144819!2d91.7877768!3d22.3643367!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30acd9835bc79051%3A0xb69949986154c96c!2sChittagong%20Khulshi%20Club%20Limited!5e0!3m2!1sen!2sbd!4v1672998016439!5m2!1sen!2sbd"
-              width="700"
-              height="293"
-              style="border: 0; padding:0px"
-              allowfullscreen=""
-              loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
-            ></iframe>
+            <iframe class="container-fluid footer-ifram"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3690.7716802873642!2d91.81181839999999!3d22.3244731!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30acd8cabac9528b%3A0x82057369a8dc9056!2s8RF6%2BVJQ%2C%20Chittagong!5e0!3m2!1sen!2sbd!4v1689222686407!5m2!1sen!2sbd"
+                width="700" height="293" style="border: 0; padding:0px" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
 </div>
