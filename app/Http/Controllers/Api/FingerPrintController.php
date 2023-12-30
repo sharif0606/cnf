ccae12fceb0631b082e15495743c26b89d8f382b
Shariful Islam <ether_sb@yahoo.com>
2023-12-30 17:03:50 +0600

    finger print api done

---
+++ app/Http/Controllers/Api/FingerPrintController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+use App\Models\OurMember;
+use Illuminate\Http\Request;
+use Exception;
+
+class FingerPrintController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function checkPrint($msno)
+    {
+        $member=OurMember::where('member_serial_no',$msno)->first();
+        if($member){
+            if($member->finger_print)
+                $data=array('error'=>'','res'=>$member->finger_print);
+            else
+                $data=array('error'=>'Finget Print not found.','res'=>'');
+        }else{
+            $data=array('error'=>'Member not found.','res'=>'');
+        }
+        
+        return response($data, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function storePrint(Request $r)
+    {
+        $msno=$r->msno;
+        $finger=$r->finger;
+        $member=OurMember::where('member_serial_no',$msno)->first();
+        if($member){
+            $member->finger_print=$finger;
+            $member->save();
+            $data=array('res'=>'Saved');
+            return response($data, 200);
+        }else{
+            $data=array('error'=>'Member not found.','res'=>'');
+            return response($data, 200);
+        }
+    }
+
+}
