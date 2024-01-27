c788aa9fcd20d11c55a9109136de3444a8b076b0
Nasim Ahmed <nashim.idb333@gmail.com>
2024-01-27 20:15:31 +0600

    hide member login

--- resources/views/frontend/app.blade.php
+++ resources/views/frontend/app.blade.php
@@ -54,8 +54,8 @@
               <a class="pt-2" href="#"><i class="bi bi-linkedin"></i></a>
               <a class="pt-2" href="#"><i class="bi bi-youtube"></i></a>
 
-              <a  id="text-right-dec" href="{{route('memLogin')}}">Member Login</a>
-              <a href="{{route('member_registration')}}" class="become-member">Become a Member</a>
+              {{-- <a  id="text-right-dec" href="{{route('memLogin')}}">Member Login</a>
+              <a href="{{route('member_registration')}}" class="become-member">Become a Member</a> --}}
             </div>
             
             <div class="row">
