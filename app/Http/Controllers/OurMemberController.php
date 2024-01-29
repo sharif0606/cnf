88c38fbecac8a04b5fe5b66b3efe512845450816
Nasim Ahmed <nashim.idb333@gmail.com>
2024-01-29 19:23:06 +0600

    update approve member

--- app/Http/Controllers/OurMemberController.php
+++ app/Http/Controllers/OurMemberController.php
@@ -77,8 +77,7 @@ class OurMemberController extends Controller
      */
     public function approvedMember(Request $request)
     {
-        // $ourmember=OurMember::select('id','name_bn','member_serial_no','member_serial_no_new','renew_serial_no','father_name','personal_phone','nid','designation_of_present_job','district','blood_group')->with('fee_collection_last')->where('approvedstatus',2)->orderBy('member_serial_no');
-        $ourmember=OurMember::where('approvedstatus',2)->orderBy('member_serial_no');
+        $ourmember=OurMember::select('id','name_bn','member_serial_no','member_serial_no_new','renew_serial_no','father_name','personal_phone','nid','designation_of_present_job','district','blood_group')->with('fee_collection_last')->where('approvedstatus',2)->orderBy('member_serial_no');
         // dd($ourmember);
         if($request->member_serial_no)
             $ourmember=$ourmember->where('member_serial_no',$request->member_serial_no);
