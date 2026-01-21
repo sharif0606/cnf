07e0f5dfdbab51da1d96c60be74c2a05bb781c94
sharif0606 <shariful0606@gmail.com>
2026-01-21 16:11:06 +0600

    update

--- app/Http/Controllers/MemberIdCardController.php
+++ app/Http/Controllers/MemberIdCardController.php
@@ -37,7 +37,7 @@ class MemberIdCardController extends Controller
         }
         if ($request->member_serial_no) {
             $cards = $cards->whereHas('member', function ($q) use ($request) {
-                $q->where('member_serial_no', 'like', '%' . $request->member_serial_no . '%');
+                $q->where('member_serial_no', $request->member_serial_no);
             });
         }
 
